da7b767bef47fbfb5fdd2fc6da79c419f6a5e288
John Positano <john.positano@listingpowertools.com>
2017-12-30 14:19:33 -0500

    getting started

--- init.js
+++ init.js
@@ -1,6 +1,14 @@
 let express = require('express');
 let app = express();
 
+app.use(
+	'/', 
+	[
+		express.static('public'),
+		express.static('node_modules'),
+	]
+);
+
 app.get('/', (req, res, next) => { res.send('hi'); });
 
 app.listen(process.env.PORT || 3000);
