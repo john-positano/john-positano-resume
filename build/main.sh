eb7ac2d327cbc8d07c8a8294a6a631730e528831
John Positano <john.positano@listingpowertools.com>
2018-01-07 14:07:18 -0500

    mur

---
+++ build/main.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# npm install
+# chmod 777 ./node_modules/browserify/index.js
+# nodejs ./node_modules/browserify/index.js ./node_modules/three-css3drenderer/index.js -o ./node_modules/three-css3drenderer/$$index.js
